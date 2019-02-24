bb6626c04804417fa4d38f644ea00cf690a17a54
Ben <ben399399@naver.com>
2019-02-25 04:53:35 +0900

    Modified TaskEvent to class

--- lib/bloc/main_page_bloc.dart
+++ lib/bloc/main_page_bloc.dart
@@ -3,25 +3,31 @@ import 'package:bloc/bloc.dart';
 import '../model/task_item.dart';
 import '../model/routine.dart';
 
-enum MainPageBlocEvent { addTask, removeTask }
+abstract class TaskEvent{}
 
-class AddTaskEvent {
+class AddTaskEvent extends TaskEvent{
   AddTaskEvent({this.title, this.color,this.routine});
   String title;
   Color color;
   Routine routine;
 }
+class RemoveTaskEvent extends TaskEvent{
+  int id;
+}
 
-class MainPageBloc extends Bloc<AddTaskEvent, List<TaskItem>> {
+class MainPageBloc extends Bloc<TaskEvent, List<TaskItem>> {
   @override
   List<TaskItem> get initialState => List<TaskItem>();
 
   @override
   Stream<List<TaskItem>> mapEventToState(
-      List<TaskItem> currentState, AddTaskEvent event) async* {
+      List<TaskItem> currentState, TaskEvent event) async* {
     if (event is AddTaskEvent){
       currentState.add(TaskItem(id:TaskItem.lastId++,title: event.title, color: event.color, routine: event.routine));
       print(this.state);
+    }
+    else if(event is RemoveTaskEvent){
+      currentState.removeWhere((i)=>i.id==event.id);
     }
   }
 }
