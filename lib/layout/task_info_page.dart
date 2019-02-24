3f5b4edafd0ad257ddf839599cd55ac564962932
Ben <ben399399@naver.com>
2019-02-24 21:26:01 +0900

    Added TaskInfoPage

---
+++ lib/layout/task_info_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../model/task_item.dart';
+
+
+class TaskInfoPage extends StatelessWidget {
+  TaskInfoPage({this.taskItem});
+  
+  final TaskItem taskItem;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
