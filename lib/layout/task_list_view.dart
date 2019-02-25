4adf2501625a785e264eaa29079755fcba3fdad0
Ben <ben399399@naver.com>
2019-02-25 20:50:33 +0900

    fast save

---
+++ lib/layout/task_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/main_page_bloc.dart';
+
+class TaskListView extends StatefulWidget {
+  TaskListView({this.mainPageBloc});
+  final MainPageBloc mainPageBloc;
+
+  @override
+  _TaskListViewState createState() => _TaskListViewState();
+}
+
+class _TaskListViewState extends State<TaskListView> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder(
+      bloc: widget.mainPageBloc,
+      builder: (BuildContext context, List state) {
+        
+      },
+    );
+  }
+}
